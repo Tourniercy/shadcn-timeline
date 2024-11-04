8a070651320dda7534c8d089c5e558e73c5cd6c5
Cyril <tourniercy@gmail.com>
2024-11-04 23:19:32 +0100

    chore: update time div width

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -16,7 +16,7 @@ const TimelineItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("relative pl-8 sm:pl-40 py-6 group", className)}
+    className={cn("relative pl-8 sm:pl-44 py-6 group", className)}
     {...props}
   />
 ));
@@ -30,10 +30,10 @@ const TimelineHeader = React.forwardRef<
     ref={ref}
     className={cn(
       "flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute " +
-        "before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[9rem] " +
+        "before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[10rem] " +
         "before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 " +
         "sm:after:left-0 after:w-2 after:h-2 after:bg-foreground after:border-4 after:box-content " +
-        "after:border-primary-foreground/95 after:rounded-full sm:after:ml-[9rem] after:-translate-x-1/2 " +
+        "after:border-primary-foreground/95 after:rounded-full sm:after:ml-[10rem] after:-translate-x-1/2 " +
         "after:translate-y-1.5",
       className,
     )}
@@ -63,7 +63,7 @@ const TimelineTime = React.forwardRef<
   return (
     <Badge
       className={cn(
-        "sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-32 h-6 mb-3 sm:mb-0",
+        "sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-36 h-6 mb-3 sm:mb-0",
         className,
       )}
       variant={variant}
