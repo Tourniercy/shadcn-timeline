38d4efbf1280863268e5ed6a994b0e87ca1a6134
Cyril <tourniercy@gmail.com>
2024-11-05 19:37:29 +0100

    chore(deps): add prettier-plugin-tailwindcss

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { cn } from "@/lib/utils";
-import { Badge } from "@/components/ui/badge";
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+import { Badge } from '@/components/ui/badge';
 
 const Timeline = React.forwardRef<
   HTMLDivElement,
@@ -8,7 +8,7 @@ const Timeline = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div ref={ref} className={className} {...props} />
 ));
-Timeline.displayName = "Timeline";
+Timeline.displayName = 'Timeline';
 
 const TimelineItem = React.forwardRef<
   HTMLDivElement,
@@ -16,11 +16,11 @@ const TimelineItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("relative pl-8 sm:pl-44 py-6 group", className)}
+    className={cn('group relative py-6 pl-8 sm:pl-44', className)}
     {...props}
   />
 ));
-TimelineItem.displayName = "TimelineItem";
+TimelineItem.displayName = 'TimelineItem';
 
 const TimelineHeader = React.forwardRef<
   HTMLParagraphElement,
@@ -29,18 +29,13 @@ const TimelineHeader = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute " +
-        "before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[10rem] " +
-        "before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 " +
-        "sm:after:left-0 after:w-2 after:h-2 after:bg-foreground after:border-4 after:box-content " +
-        "after:border-primary-foreground/95 after:rounded-full sm:after:ml-[10rem] after:-translate-x-1/2 " +
-        "after:translate-y-1.5",
-      className,
+      'mb-1 flex flex-col items-start before:absolute before:left-2 before:h-full before:-translate-x-1/2 before:translate-y-3 before:self-start before:bg-slate-300 before:px-px after:absolute after:left-2 after:box-content after:h-2 after:w-2 after:-translate-x-1/2 after:translate-y-1.5 after:rounded-full after:border-4 after:border-primary-foreground/95 after:bg-foreground group-last:before:hidden sm:flex-row sm:before:left-0 sm:before:ml-[10rem] sm:after:left-0 sm:after:ml-[10rem]',
+      className
     )}
     {...props}
   />
 ));
-TimelineHeader.displayName = "TimelineHeader";
+TimelineHeader.displayName = 'TimelineHeader';
 
 const TimelineTitle = React.forwardRef<
   HTMLDivElement,
@@ -48,23 +43,23 @@ const TimelineTitle = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-xl font-bold text-primary", className)}
+    className={cn('text-xl font-bold text-primary', className)}
     {...props}
   >
     {children}
   </div>
 ));
-TimelineTitle.displayName = "TimelineTitle";
+TimelineTitle.displayName = 'TimelineTitle';
 
 const TimelineTime = React.forwardRef<
   HTMLDivElement,
   React.ComponentProps<typeof Badge>
->(({ className, variant = "default", ...props }) => {
+>(({ className, variant = 'default', ...props }) => {
   return (
     <Badge
       className={cn(
-        "sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-36 h-6 mb-3 sm:mb-0",
-        className,
+        'left-0 mb-3 inline-flex h-6 w-36 translate-y-0.5 items-center justify-center text-xs font-semibold uppercase sm:absolute sm:mb-0',
+        className
       )}
       variant={variant}
       {...props}
@@ -73,7 +68,7 @@ const TimelineTime = React.forwardRef<
     </Badge>
   );
 });
-TimelineTime.displayName = "TimelineTime";
+TimelineTime.displayName = 'TimelineTime';
 
 const TimelineDescription = React.forwardRef<
   HTMLDivElement,
@@ -81,11 +76,11 @@ const TimelineDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("text-muted-foreground", className)}
+    className={cn('text-muted-foreground', className)}
     {...props}
   />
 ));
-TimelineDescription.displayName = "TimelineDescription";
+TimelineDescription.displayName = 'TimelineDescription';
 
 export {
   Timeline,
